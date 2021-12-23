53597d1bdc4be3afb7e31ccc21364779a0444371
Karym Gharby <Karimgharbi0@gmail.com>
2021-12-23 03:02:25 +0100

    added map :sparkles:

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import ReactMapGL, { Marker,NavigationControl,Popup } from "react-map-gl";
 import './Map.css'
-import env from'
+
 import {MdPlace} from 'react-icons/md'
 export default function Map() {
   const [markerCoord, setMarkerCoord] = useState(null);
 
+  const [showPopup, setShowPopup] = useState(false);
   
 
   const navControlStyle= {
@@ -14,20 +15,20 @@ export default function Map() {
   };
 
   const [viewport, setViewport] = useState({
-     width: "40vw",
+     width: "30vw",
     height: "100vh",
-    latitude: 36,
-    longitude: 10,
-    zoom: 18,
+    latitude: 36.710161050637595,
+    longitude: 10.428860882171598,
+    zoom: 12
   });
 
   useEffect(() => {
     //get user location
     navigator.geolocation.getCurrentPosition((pos) => {
       setViewport({
-         width: "40vw",
+         width: "30vw",
         height: "100vh",
-        zoom: 7,
+        zoom: 12,
         latitude: pos.coords.latitude,
         longitude: pos.coords.longitude,
       });
@@ -64,16 +65,64 @@ export default function Map() {
           </Marker>
           
         )}
+
+            {/* Show Popup Card */}
+
+         <Popup
+          latitude={36.710161050637595}
+          longitude={10.428860882171598}
+          closeButton={true}
+          closeOnClick={false}
+          onClose={() => setShowPopup(false)}
+          anchor="left" >
+          <div className="card">
+                <img height="40px" alt="Keys" className="card__image"
+                 src="https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F1026205392%2FBeautiful-luxury-home-exterior-at-twilight%2F960x0.jpg%3Ffit%3Dscale"/>
+                 <p>🚩</p>
+                </div>
+        </Popup>
+        <Popup
+          latitude={36.5}
+          longitude={10.4}
+          closeButton={true}
+          className="card"
+          closeOnClick={false}
+          onClose={() => setShowPopup(false)}
+          anchor="left" >
+          <div className="card_info">
+            <img height="40px" alt="Keys" className="card__image" src="https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F1026205392%2FBeautiful-luxury-home-exterior-at-twilight%2F960x0.jpg%3Ffit%3Dscale"/>
+          </div>
+          <div className="triangle"/>
+        </Popup>
+        <Popup
+          latitude={36.4}
+          longitude={10.3}
+          closeButton={true}
+          closeOnClick={false}
+          onClose={() => setShowPopup(false)}
+          anchor="left" >
+          <div className="card">
+                <img height="40px" alt="Keys" className="card__image"
+                 src="https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F1026205392%2FBeautiful-luxury-home-exterior-at-twilight%2F960x0.jpg%3Ffit%3Dscale"/>
+                 <p>🚩</p>
+                </div>
+        </Popup>
         {/* <Popup
                 
-                latitude={markerCoord.lat}
-                longitude={markerCoord.lng}
+                latitude={36}
+                longitude={10}
                 closeButton={true}
                 closeOnClick={false}
-                onClose={() => setCurrentPlaceId(null)}
-                anchor="left"
-              ></Popup>
-         */}
+               
+                anchor="bottom"
+              >
+                <div className="card">
+                <img height="60px"alt="Keys" className="card__image"
+                 src="https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F1026205392%2FBeautiful-luxury-home-exterior-at-twilight%2F960x0.jpg%3Ffit%3Dscale"/>
+                 <h1>aa</h1>
+                </div>
+              </Popup> */}
+        
         <NavigationControl style={navControlStyle} />
       </ReactMapGL>
     </div>
