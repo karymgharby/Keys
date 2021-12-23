4b2ace5103d3a0739fd2c676655bfe4e328e8e30
WaelNalouti <wael99g1@gmail.com>
2021-12-23 22:12:28 +0100

    :sparkles: imported the map component

--- src/pages/discover/Discover.js
+++ src/pages/discover/Discover.js
@@ -1,8 +1,11 @@
+import "./Discover.css";
 import Sidebar from "../../components/sidebar/Sidebar";
+import Map from "../../components/map/Map";
 export default function Discover() {
   return (
     <div className="discover">
       <Sidebar />
+      <Map />
     </div>
   );
 }
