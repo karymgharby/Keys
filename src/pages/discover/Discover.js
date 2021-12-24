e88ae919dcc550affece35921734fc79c2f6c82e
WaelNalouti <wael99g1@gmail.com>
2021-12-24 02:17:19 +0100

    :sparkles: imported the post component

--- src/pages/discover/Discover.js
+++ src/pages/discover/Discover.js
@@ -6,7 +6,41 @@ export default function Discover() {
   return (
     <div className="discover">
       <Sidebar />
-      <Post postLocalisation="" postTitle="" postDescription="" postPrice="" />
+      <div className="discover__posts">
+        <h1>Discover</h1>
+        <p>Discover {">"} Houses</p>
+
+        <Post
+          postLocalisation="25th st , Manouba , Tunis "
+          postTitle="Spacious and Luminous "
+          postDescription="Great for entertaining: spacious, updated 2 bedroom, 1 bathroom apartment in Lakeview, available May 1st. Close to nightlife with private backyard. "
+          postPrice="550"
+        />
+        <Post
+          postLocalisation="25th st , Manouba , Tunis "
+          postTitle="Spacious and Luminous "
+          postDescription="Great for entertaining: spacious, updated 2 bedroom, 1 bathroom apartment in Lakeview, available May 1st. Close to nightlife with private backyard. "
+          postPrice="550"
+        />
+        <Post
+          postLocalisation="25th st , Manouba , Tunis "
+          postTitle="Spacious and Luminous "
+          postDescription="Great for entertaining: spacious, updated 2 bedroom, 1 bathroom apartment in Lakeview, available May 1st. Close to nightlife with private backyard. "
+          postPrice="550"
+        />
+        <Post
+          postLocalisation="25th st , Manouba , Tunis "
+          postTitle="Spacious and Luminous "
+          postDescription="Great for entertaining: spacious, updated 2 bedroom, 1 bathroom apartment in Lakeview, available May 1st. Close to nightlife with private backyard. "
+          postPrice="550"
+        />
+        <Post
+          postLocalisation="25th st , Manouba , Tunis "
+          postTitle="Spacious and Luminous "
+          postDescription="Great for entertaining: spacious, updated 2 bedroom, 1 bathroom apartment in Lakeview, available May 1st. Close to nightlife with private backyard. "
+          postPrice="550"
+        />
+      </div>
       <Map />
     </div>
   );
