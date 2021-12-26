0122af8cb9a479f8baa7fd155710dd3266fdf1c4
WaelNalouti <wael99g1@gmail.com>
2021-12-26 14:51:52 +0100

    added route structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 import Discover from "./pages/discover/Discover";
+import { Route, Link } from "react-router-dom";
+import Sidebar from "./components/sidebar/Sidebar";
+
 function App() {
   return (
-    <div className="App">
-      <div className="">
+    <div className="app">
+      <Sidebar />
+      <div className="app__main">
         <Discover />
       </div>
     </div>
