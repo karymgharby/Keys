f0875a636fc5ce8afbf4c0482d7be56d113f59ef
WaelNalouti <wael99g1@gmail.com>
2021-12-26 13:21:59 +0100

    changed the post image link

--- src/pages/discover/Discover.js
+++ src/pages/discover/Discover.js
@@ -15,30 +15,7 @@ export default function Discover() {
           postTitle="Spacious and Luminous "
           postDescription="Great for entertaining: spacious, updated 2 bedroom, 1 bathroom apartment in Lakeview, available May 1st. Close to nightlife with private backyard. "
           postPrice="550"
-        />
-        <Post
-          postLocalisation="25th st , Manouba , Tunis "
-          postTitle="Spacious and Luminous "
-          postDescription="Great for entertaining: spacious, updated 2 bedroom, 1 bathroom apartment in Lakeview, available May 1st. Close to nightlife with private backyard. "
-          postPrice="550"
-        />
-        <Post
-          postLocalisation="25th st , Manouba , Tunis "
-          postTitle="Spacious and Luminous "
-          postDescription="Great for entertaining: spacious, updated 2 bedroom, 1 bathroom apartment in Lakeview, available May 1st. Close to nightlife with private backyard. "
-          postPrice="550"
-        />
-        <Post
-          postLocalisation="25th st , Manouba , Tunis "
-          postTitle="Spacious and Luminous "
-          postDescription="Great for entertaining: spacious, updated 2 bedroom, 1 bathroom apartment in Lakeview, available May 1st. Close to nightlife with private backyard. "
-          postPrice="550"
-        />
-        <Post
-          postLocalisation="25th st , Manouba , Tunis "
-          postTitle="Spacious and Luminous "
-          postDescription="Great for entertaining: spacious, updated 2 bedroom, 1 bathroom apartment in Lakeview, available May 1st. Close to nightlife with private backyard. "
-          postPrice="550"
+          houseImage="https://images.unsplash.com/photo-1600607686527-6fb886090705?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
         />
       </div>
       <Map />
