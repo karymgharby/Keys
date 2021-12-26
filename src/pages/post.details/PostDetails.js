b1c5d1ac80436dc9a8418ad23e790415e99a4704
WaelNalouti <wael99g1@gmail.com>
2021-12-26 16:39:04 +0100

    :tada: added component structure

--- src/pages/post.details/PostDetails.js
+++ src/pages/post.details/PostDetails.js
@@ -1,5 +1,22 @@
+import Map from "../../components/map/Map";
 import "./PostDetails.css";
 
 export default function PostDetails() {
-  return <h1>Post Details Page</h1>;
+  return (
+    <div className="Postdeatils">
+      <div className="Postdeatils__post">
+        <h1 className="page__title">Discover</h1>
+        <p className="page__navigation">
+          Discover {">"} Houses {">"} Details
+        </p>
+        {/* PostPreview */}
+
+        <div className="Postdetails__more">
+          {/* Post details */}
+          {/* Post reviews */}
+        </div>
+      </div>
+      <Map />
+    </div>
+  );
 }
